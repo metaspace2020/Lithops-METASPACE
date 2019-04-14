11ad8ad665a130c87a9413853d71c8575e8009f8
Lachlan Stuart <26366936+LachlanStuart@users.noreply.github.com>
2019-04-14 16:30:55 +0200

    Add CLI

---
+++ annotation_pipeline/__main__.py
@@ -0,0 +1,47 @@
+import argparse
+import json
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+from annotation_pipeline.pipeline import annotate_dataset
+
+
+def annotate(args):
+    config = json.load(args.config)
+    input_config = json.load(args.input)
+    input_data = input_config['dataset']
+    input_db = input_config['molecular_db']
+    output = Path(args.output) if not args.no_output else None
+
+    if output and not output.exists():
+        output.mkdir(parents=True, exist_ok=True)
+
+    formula_scores_df, formula_images = annotate_dataset(config, input_data, input_db)
+
+    if output:
+        formula_scores_df.to_pickle(output / 'formula_scores_df.pickle')
+        for key, image_set in formula_images.items():
+            for i, image in enumerate(image_set):
+                plt.imsave(output / f'{key}_{i}.png', image.toarray())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run colocalization jobs',
+                                     usage='python -m annotation_pipeline annotate [input_config.json] [output path]')
+    parser.add_argument('--config', type=argparse.FileType('r'), default='config.json', help='config.json path')
+
+    subparsers = parser.add_subparsers(title='Sub-commands', dest='action')
+    subparsers.required = True
+
+    annotate_parser = subparsers.add_parser('annotate')
+    annotate_parser.add_argument('input', type=argparse.FileType('r'), default='input_config.json', nargs='?',
+                                 help='input_config.json path')
+    annotate_parser.add_argument('output', default='output', nargs='?', help='directory to write output files')
+    annotate_parser.add_argument('--no-output', action="store_true", help='prevents outputs from being written to file')
+    annotate_parser.set_defaults(func=annotate)
+    subparsers.add_parser('process')
+    subparsers.add_parser('convert')
+
+    args = parser.parse_args()
+    args.func(args)
+
---
+++ annotation_pipeline/pipeline.py
@@ -0,0 +1,30 @@
+import logging
+
+from .dataset import read_dataset_coords, real_pixel_indices
+from .molecular_db import store_centroids_database
+from .dataset_segmentation import generate_segm_intervals, split_spectra_into_segments, clean_segments
+from .annotation import annotate_spectra, merge_annotation_results
+
+logger = logging.getLogger(name='annotation_pipeline')
+
+
+def annotate_dataset(config, input_data, input_db,segm_n = 256):
+
+    logger.info('Storing centroids database')
+    store_centroids_database(config, input_db)
+
+    spectra_coords = read_dataset_coords(config, input_data)
+    pixel_indices, nrows, ncols = real_pixel_indices(spectra_coords)
+
+    logger.info('Generating segments')
+    segm_intervals = generate_segm_intervals(config, input_db, segm_n)
+    split_spectra_into_segments(config, input_data, segm_n, segm_intervals)
+
+    logger.info('Annotating')
+    results = annotate_spectra(config, input_data, input_db, segm_n, pixel_indices, nrows, ncols)
+    formula_scores_df, formula_images = merge_annotation_results(results)
+
+    logger.info('Cleaning up')
+    clean_segments(config, input_data)
+
+    return formula_scores_df, formula_images
