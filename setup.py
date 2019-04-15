09c6c1cc6183ff825a80dbe707205c399b0a0277
LachlanStuart <26366936+LachlanStuart@users.noreply.github.com>
2019-04-15 13:20:00 +0200

    Move annotation code out of notebook (#12)
    * Extract pipeline code from notebook into Python module
    * Extract input data config into separate file
    * Add CLI
    * Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import sys
+from setuptools import setup, find_packages
+
+from annotation_pipeline import __version__
+
+assert (3, 6) <= sys.version_info < (3, 7), 'Python 3.6 is required'
+
+setup(name='pywren-annotation-pipeline',
+      version=__version__,
+      description='PyWren-powered METASPACE annotation pipeline',
+      url='https://github.com/metaspace2020/pywren-annotation-pipeline',
+      packages=find_packages(),
+      install_requires=[
+          "pywren-ibm-cloud>=1.0.8",
+          "pandas>=0.19.2",
+          "pyImagingMSpec==0.1.4",
+          "cpyImagingMSpec==0.3.2",
+          "pyMSpec==0.1.2",
+          "cpyMSpec==0.4.2",
+          "pyimzML==1.2.5"
+      ])
